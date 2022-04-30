aa98c869a7ca2862fa41769b7df481d936f69f37
andr3103 <91419981+andr3103@users.noreply.github.com>
2022-04-30 12:33:03 +0200

    Add files via upload

---
+++ salva colore pulsante/salva colore pulsante/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace salva_colore_pulsante
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.opn_button = new System.Windows.Forms.Button();
+            this.sve_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.color_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // opn_button
+            // 
+            this.opn_button.Location = new System.Drawing.Point(31, 111);
+            this.opn_button.Name = "opn_button";
+            this.opn_button.Size = new System.Drawing.Size(105, 32);
+            this.opn_button.TabIndex = 0;
+            this.opn_button.Text = "Open";
+            this.opn_button.UseVisualStyleBackColor = true;
+            this.opn_button.Click += new System.EventHandler(this.opn_button_Click);
+            // 
+            // sve_button
+            // 
+            this.sve_button.Location = new System.Drawing.Point(31, 147);
+            this.sve_button.Name = "sve_button";
+            this.sve_button.Size = new System.Drawing.Size(105, 32);
+            this.sve_button.TabIndex = 1;
+            this.sve_button.Text = "Save";
+            this.sve_button.UseVisualStyleBackColor = true;
+            this.sve_button.Click += new System.EventHandler(this.sve_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome file:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Percorso file:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // color_button
+            // 
+            this.color_button.Location = new System.Drawing.Point(31, 185);
+            this.color_button.Name = "color_button";
+            this.color_button.Size = new System.Drawing.Size(105, 32);
+            this.color_button.TabIndex = 6;
+            this.color_button.Text = "Color";
+            this.color_button.UseVisualStyleBackColor = true;
+            this.color_button.Click += new System.EventHandler(this.color_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 108);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 109);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(271, 39);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Questo form permette di salvare e aprire file \r\ntestuali, il pulsante \"color\" si " +
+    "colora del colore scritto nel \r\ntextbox una volta premuto\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 319);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.color_button);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sve_button);
+            this.Controls.Add(this.opn_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button opn_button;
+        private System.Windows.Forms.Button sve_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button color_button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ salva colore pulsante/salva colore pulsante/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace salva_colore_pulsante
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sve_button_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog chiudi = new SaveFileDialog();
+            if (chiudi.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter scrvi = new StreamWriter(File.Create(chiudi.FileName));
+                scrvi.Write(textBox1.Text);
+                scrvi.Close();
+                scrvi.Dispose();
+
+            }
+        }
+
+        private void opn_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog apri = new OpenFileDialog();
+            apri.Title = "sciegli un file da aprire";
+            apri.Filter = "Text File|*.txt|All Files|*.*";
+            if (apri.ShowDialog() == DialogResult.OK)
+            {
+                label3.Text = apri.SafeFileName;
+                label4.Text = apri.FileName;
+                StreamReader leggi = new StreamReader(apri.FileName);
+                textBox1.Text = leggi.ReadToEnd();
+                leggi.Close();
+                apri.Dispose();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void color_button_Click(object sender, EventArgs e)
+        {
+
+            string colore = textBox1.Text;
+            if (colore == "")
+            {
+                MessageBox.Show("Il campo è vuoto");
+                color_button.BackColor = Color.Transparent;
+            }
+            else
+            {
+                color_button.BackColor = Color.FromName(colore);
+            }
+
+        }
+    }
+}
